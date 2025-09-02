4453db94e49df361d5e3eb7ff1044535f2012a39
Ivin <vaz.ivin@gmail.com>
2025-09-02 20:34:06 -0300

    Adicionando práticas 03

---
+++ praticas/pratica03/test/calcularMediaAluno.test.js
@@ -0,0 +1,29 @@
+const { calcularMediaAluno } = require('../src/calcularMediaAluno');
+
+test("Teste Undefined", function(){
+    expect(calcularMediaAluno).toBeDefined();
+    expect(()=>calcularMediaAluno(undefined,1,2)).toThrow("Notas a1 ou a2 não informadas");
+    expect(()=>calcularMediaAluno(2,undefined,2)).toThrow("Notas a1 ou a2 não informadas");
+});
+
+test("a1,a2 Teste Negativo", function(){
+    expect(calcularMediaAluno).toBeDefined();
+    expect(()=>calcularMediaAluno(-1,1,2)).toThrow("Notas a1 ou a2 não podem ser negativas");
+    expect(()=>calcularMediaAluno(1,-1,2)).toThrow("Notas a1 ou a2 não podem ser negativas");
+});
+
+test("a3 Teste Negativo", function(){
+    expect(calcularMediaAluno).toBeDefined();
+    expect(()=>calcularMediaAluno(1,1,-2)).toThrow("Nota a3 não pode ser negativa");
+});
+
+test("a3 Nulo", function(){
+    expect(calcularMediaAluno).toBeDefined();
+    expect(calcularMediaAluno(6,9,undefined)).toBeCloseTo(3.9);
+});
+
+test("melhor nota", function(){
+    expect(calcularMediaAluno).toBeDefined();
+    expect(calcularMediaAluno(9,3,6)).toBeCloseTo(3.6);
+    expect(calcularMediaAluno(3,9,6)).toBeCloseTo(3.9);
+});

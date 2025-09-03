53ef8ef49f6a3d71f37e6fd96f594d0e48b6d326
Ivin <vaz.ivin@gmail.com>
2025-09-02 21:22:14 -0300

    Corrigindo lógica de teste de negativo em práticas 03

--- praticas/pratica03/src/calcularMediaAluno.js
+++ praticas/pratica03/src/calcularMediaAluno.js
@@ -1,7 +1,7 @@
 function calcularMediaAluno(a1, a2, a3){
     if(a1 == undefined || a2 == undefined) throw Error("Notas a1 ou a2 não informadas");
-    if(a1 <= -1 || a2 <= -1) throw Error("Notas a1 ou a2 não podem ser negativas");
-    if(a3 <= -1) throw Error("Nota a3 não pode ser negativa");
+    if(a1 < 0 || a2 < 0) throw Error("Notas a1 ou a2 não podem ser negativas");
+    if(a3 < 0) throw Error("Nota a3 não pode ser negativa");
     if(a3 === undefined)  return ((a1*0.4)+(a2*0.6));
     return Math.max((((a1*0.4)+(a3*0.6))),(((a2*0.6)+(a3*0.4))));
 };

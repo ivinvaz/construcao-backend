e0c00e925d8ba8b1991d1b43e95b1df302b4e8e0
Ivin <vaz.ivin@gmail.com>
2025-09-16 21:48:16 -0300

    Terminando aula06

--- aulas/aula06/app.js
+++ aulas/aula06/app.js
@@ -16,4 +16,22 @@ app.use(cookieParser());
 
 app.use('/', indexRouter);
 
+const tarefas = [];
+app.get('/tarefas', (req,res)=>{
+    res.json('');
+})
+
+app.post('/tarefas', (req,res)=>{
+    const novaTarefa = {id: tarefas.length + 1, ...req.body};
+    res.status(201).json(novaTarefa);
+    tarefas.push(novaTarefa);
+})
+
+app.get('/tarefas/:id', (req,res)=>{
+    const { id } = req.params;
+    const tarefasEncontrada = tarefas.find(item => item.id === parseInt(id)) 
+    if(tarefasEncontrada) res.json(tarefasEncontrada)
+    else res.status(404).json({msg:'Tarefa não encontrada'})
+})
+
 module.exports = app;
